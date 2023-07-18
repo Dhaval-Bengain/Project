0c0d0986bc02c27d440844ef5e96622f308e106a
Dhaval-Bengain <139906848+Dhaval-Bengain@users.noreply.github.com>
2023-07-18 23:53:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,48 +1,53 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
 (async () => {
-  // Launch headful browser
   const browser = await puppeteer.launch({ headless: false });
-
-  // Open a new tab and navigate to the website
   const page = await browser.newPage();
-  await page.setViewport({ width: 1366, height: 768});                                   //open page in max size
-  await page.goto('https://swap.defillama.com/'); 
+  await page.setViewport({ width: 1366, height: 768 });
+  await page.goto("https://swap.defillama.com/");
 
+// Fill the form
 
-  // Fill the form
+  await page.click(".css-1d8n9bt");
+  const ArbitrumOption = (await page.$x("//div[contains(text(),'Arbitrum')]"))[0]; // selecting Arbitrum Option
+  await ArbitrumOption.click();
 
-  
+// deleting predefine input
+  const inputSelector = 'input[class="chakra-input css-lv0ed5"]';
+  await page.focus(inputSelector);
+  await page.keyboard.down("ControlLeft");
+  await page.keyboard.press("KeyA");
+  await page.keyboard.press("Backspace");
+  await page.keyboard.up("ControlLeft");
 
-  await page.locator('input[class="chakra-input css-lv0ed5"]').fill('12');                //you sell input
+// typing input 
+  await page.type(inputSelector, "12");
 
-  //token (you sell)
-  await page.locator('span[class="chakra-text css-sys4p8"]').click();                     // select token (you sell)
-  await page.locator('input[class="chakra-input css-s1d1f4"]').fill('WBTC');              // filling the input
-  const btcOption = await page.waitForSelector('div ::-p-text(Wrapped BTC (WBTC))');    // selecting BTC option
+  // Token (you sell)
+  await page.click('span[class="chakra-text css-sys4p8"]');                           // select token (you sell)
+  await page.type('input[class="chakra-input css-s1d1f4"]', "WBTC");                  // filling the input
+  const btcOption = await page.waitForSelector( "div ::-p-text(Wrapped BTC (WBTC))"); // selecting BTC option
   await btcOption.click();
 
-
-  // select token (you buy)
-  //  Note => insted of class use spam to capture the botton otherwise first button will be selected
-  
-  const btn = (await page.$x("//span[contains(text(),'Select Token')]"))[0];          //button click   
+  // Select token (you buy)
+  const btn = (await page.$x("//span[contains(text(),'Select Token')]"))[0]; // button click
   await btn.click();
-  await page.locator('input[class="chakra-input css-s1d1f4"]').fill('usdc');          // filling the input
-  const usdcOption = await page.waitForSelector('div ::-p-text(USD Coin (USDC))');    // udsc element capture
+  await page.type('input[class="chakra-input css-s1d1f4"]', "usdc"); // filling the input
+  const usdcOption = await page.waitForSelector('div ::-p-text("USD Coin (USDC)")'); // usdc element capture
   await usdcOption.click();
 
 
+  await page.waitForSelector(".sc-d413ea6e-0", {visible: true,timeout: 4000,}); // wait for right section to load
+ 
+  // setTimeout is used when site takes time to load
+  setTimeout(() => {
+    page.evaluate(() => {
+      let elements = document.getElementsByClassName("sc-d413ea6e-0");
+      if (elements.length > 0) elements[1].click();
+    });
+  }, 5000);
+
   
-await page.waitForSelector('.sc-d413ea6e-0', { timeout: 50000 });                    // wait for right section to lode
-await page.evaluate(() => {                                                          // evaluate is used to write vanilla javascript
-          		let elements = document.getElementsByClassName('sc-d413ea6e-0');       // select all div containing 'sc-d413ea6e-0' class
-          		if (elements.length > 0)
-  				elements[1].click()                                                        // selecting 2nd element
-      		})
-
-
-
   // Keep the browser window open
   await new Promise(() => { });
 })();
